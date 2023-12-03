2a52d94cc393ae28cf26b80ae1cb45ea5c861a4b
Kevin Segovia <kevinsegovia@Kevins-MacBook-Pro.local>
2023-12-03 14:44:15 -0700

    Made a loading animation

--- manai.py
+++ manai.py
@@ -2,17 +2,23 @@ import man_find
 import os 
 import pinecone 
 from tqdm import tqdm
-import threading
 import time 
+import threading
 from dotenv import load_dotenv, find_dotenv
 from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import DirectoryLoader, TextLoader
 from langchain.chat_models import ChatOpenAI
 
-def simulate_long_task():
-     for _ in tqdm(range(50), desc="Reading man page", unit="step"):
-            time.sleep(0.1)
+def loading_animation(event): 
+    symbols = ['|', '/', '-', '\\']
+    current = 0
+
+    while not event.is_set():
+          print(symbols[current], end='\r')
+          time.sleep(0.1)
+          current = (current + 1) % len(symbols)
+    print("Done")
 
 load_dotenv(find_dotenv(), override=True)
 llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.5, max_tokens=1024)
@@ -21,7 +27,8 @@ llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.5, max_tokens=1024)
 def chunk_data(data, chunk_size=1024):
     from langchain.text_splitter import RecursiveCharacterTextSplitter
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
-    chunks = text_splitter.split_documents(data)
+    print("Chunking documents")
+    chunks = [chunk for chunk in tqdm(text_splitter.split_documents(data), desc="Chunking documents")]
     return chunks
 
 # upload the chunks to pinecone 
@@ -29,37 +36,62 @@ def insert_or_fetch_embedding(index_name, chunks):
     embeddings = OpenAIEmbeddings()
     pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment=os.getenv('PINECONE_ENV'))
 
-    if index_name in pinecone.list_indexes():
-        # index already exists, fetch it 
-        vector_store = Pinecone.from_existing_index(index_name, embeddings)
-    else: 
+    print("Creating index...")
+    #start loading animation
+    stop_loading_event = threading.Event()
+    loading_event = threading.Thread(target=loading_animation, args=(stop_loading_event,))
+    loading_event.start()
 
-        # create new index 
-        pinecone.create_index(name=index_name, dimension=1536, metric='cosine')
+    try: 
+        if index_name in pinecone.list_indexes():
+            # index already exists, fetch it 
+            vector_store = Pinecone.from_existing_index(index_name, embeddings)
+        else: 
+            # create new index 
+            pinecone.create_index(name=index_name, dimension=1536, metric='cosine')
 
-        vector_store = Pinecone.from_documents(chunks, embeddings, index_name=index_name)
+            vector_store = Pinecone.from_documents(chunks, embeddings, index_name=index_name)
+    finally: 
+         #stop loading animation 
+         stop_loading_event.set()
+         loading_event.join()
     return vector_store
 
 # delete index in pinecone (free tier needs this)
 def delete_pinecone_index(index_name='all'):
     pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment=os.getenv('PINECONE_ENV'))
+    print("Deleting index...")
+    stop_loading_event = threading.Event()
+    loading_event = threading.Thread(target=loading_animation, args=(stop_loading_event,))
+    loading_event.start()
+    try:
+        if index_name == 'all':
+            indexes = pinecone.list_indexes()
+            for index in indexes:
+                pinecone.delete_index(index)
+        else:
+            pinecone.delete_index(index_name)
 
-    if index_name == 'all':
-          indexes = pinecone.list_indexes()
-          for index in indexes:
-               pinecone.delete_index(index)
-    else:
-        pinecone.delete_index(index_name)
+    finally: 
+        stop_loading_event.set()
+        loading_event.join()
 
 def asked_question(vector_store, message):
     from langchain.chains import RetrievalQA
-     
-    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.5)
+    print("Coming up with an answer...")
+    stop_loading_event = threading.Event()
+    loading_event = threading.Thread(target=loading_animation, args=(stop_loading_event,))
+    loading_event.start() 
+    try:
+        llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.5)
 
-    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': 3})
-    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+        retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': 3})
+        chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
-    answer = chain.run(message)
+        answer = chain.run(message)
+    finally: 
+        stop_loading_event.set()
+        loading_event.join()
     return answer
 
 def command_message(command, message):
